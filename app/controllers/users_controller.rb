3645c52e9904d4f71752be8325a003052f681f99
ghazalshahzadi <ghazalshahzadi1@gmail.com>
2022-03-05 10:34:55 +0500

    add edit,update,and partial functionality in existing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,20 @@
       else
          render 'new'
       end
+      end
+    def edit
+    @user = User.find(params[:id])
     end
-
-private
+    def update
+    @user = User.find(params[:id])
+      if @user.update(user_params)
+       flash[:success] = "Your account was updated successfully"
+         redirect_to articles_path
+       else
+      render 'edit'
+      end
+      end
+   private
     def user_params
       params.require(:user).permit(:username, :password, :email)
    end
