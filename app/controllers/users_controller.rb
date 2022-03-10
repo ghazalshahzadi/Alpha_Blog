b02000b0d2a3b76fd0e4f8d20e4c789b066baaaf
ghazalshahzadi <ghazalshahzadi1@gmail.com>
2022-03-10 10:19:36 +0500

    add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,10 @@
     end
     def create
       @user = User.new(user_params)
-      if @user.save
+      if @user.save 
+        session[:user_id] = @user_id
          flash[:success] = "Welcome to the alpha-blog #{@user.username}"
-         redirect_to articles_path
+         redirect_to user_path(@user)
       else
          render 'new'
       end
